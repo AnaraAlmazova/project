fb0d54ae011e48a1eb4f626dd21c86b764f75d9a
Анара Алмазова <anuyka.almaz@gmail.com>
2022-09-24 14:43:48 +0300

    add project

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+       // String[] names = {"Anara", "Aidai", "Mira"};
+       // for (String a : names) {
+        //    System.out.println("men seni jakwy korom " + a);
+     //   }
+
+        String soz=" Kyrgyzstan";
+        System.out.println(soz.toUpperCase());
+        System.out.println(soz.trim());
+        System.out.println(soz.indexOf("a"));
+        System.out.println(soz.substring(8));
+        System.out.println(soz.contains("A"));
+
+    }
+
+
+}
